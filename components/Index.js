5511ff84edbfdc2150e83a58393aef034b55638b
jhon-swai <jhonswai@gmail.com>
2020-10-20 14:44:44 +0300

    added the navigation stack

--- components/Index.js
+++ components/Index.js
@@ -1,9 +1,31 @@
+import 'react-native-gesture-handler';
 import React from 'react'
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack'
 
 import Diagnosis from './screens/Diagnosis'
+import Results from './screens/Results'
+
+const Stack = createStackNavigator()
 
 export default function Index(){
     return (
-        <Diagnosis/>
+        <NavigationContainer>
+            <Stack.Navigator>
+                <Stack.Screen
+                    name="Diagnosis"
+                    component={Diagnosis}
+                />
+
+                <Stack.Screen
+                    name="Results"
+                    component={Results}
+                />
+                    
+                
+            </Stack.Navigator>
+            
+        </NavigationContainer>
+        
     )
 }
